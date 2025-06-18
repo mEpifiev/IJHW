34207698341a445ebb233beb16cf5b47dc114a36
Marlen Epifiev <epifiev@gmail.com>
2025-06-18 19:20:40 +0300

    fix errors

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,29 +7,18 @@ public class Spawner : MonoBehaviour
     private const int SizeDivider = 2;
     private const float SplitChanceDivider = 2f;
 
-    [SerializeField] private Raycaster _raycaster;
     [SerializeField] private Cube _cubePrefab;
 
-    private void OnEnable()
-    {
-        _raycaster.Hited += Spawn;
-    }
-
-    private void OnDisable()
-    {
-        _raycaster.Hited -= Spawn;
-    }
-
-    private void Spawn(Cube parent)
+    public void Spawn(Cube parent)
     {
         int count = Random.Range(MinCount, MaxCount + 1);
+
+        Vector3 newScale = parent.transform.localScale / SizeDivider;
+        float newSplitChance = parent.CurrentSplitChance / SplitChanceDivider;
 
         for (int i = 0; i < count; i++)
         {
             Cube newCube = Instantiate(_cubePrefab, parent.transform.position, Quaternion.identity);
-
-            Vector3 newScale = parent.transform.localScale / SizeDivider;
-            float newSplitChance = parent.CurrentSplitChance / SplitChanceDivider;
 
             newCube.Initialize(newScale, newSplitChance);
         }
